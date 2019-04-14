f32866cd6a7f224b102dbafb1b7f9810531c6254
xbugz <49458357+xbugz@users.noreply.github.com>
2019-04-14 13:37:47 -0600

    Update hash.hpp

--- HPPfiles/hash.hpp
+++ HPPfiles/hash.hpp
@@ -1,24 +1,47 @@
-/*
-Starter HPP File for Hash Tables.
-DISCLAIMER: We recommend everyone to at least have these functions implemented properly.
-
-For the exams the variable type might change form int to char / any other custom type.
-You will also have extra functions which will be the main exam problems. These will just be added to this hpp file and it will be given to you during your exam
-*/
-
-
-
 #ifndef HASH_HPP
 #define HASH_HPP
 
-
 using namespace std;
 
-// Struct for a linked list node
-struct node
+// Struct for a player
+struct player
 {
-    int data; // data to be stored in the node
-    struct node* next; // pointer to the next node
+  int age; // player age
+  string position; // player position
+  int G;  // games played
+  int GS; // games started
+  int MP; // Minutes played
+  int FG; // Field goals
+  int FGA; // field goal attempts
+  float FGP; // field goal percentage
+  int TP; // three pointers made
+  int TPA; // three pointers attemp
+  float TPPer; // three pointer percentage
+  int TWOP; // Two pointers field goal
+  int TWOPA; // two point field goal attemps
+  float TWOPPer; // two point field goal percentage
+  int FT; // free throws
+  int FTA; // free throw attempts
+  float FTper; // free throw percentage
+  int ORB; // offensive reb
+  int DRB; // defensive rebounds
+  float ORBpg; // offensive rebounds per game
+  float DRBpg; // defensive rebounds per game
+  int TRB; // total rebounds
+  float TRBpg; // total rebounds per game
+  int ASS; // total assists
+  float ASSpg; // assisst per games
+  int STL; // steals
+  float STLpg; // steals per game
+  int BLK; // blocks
+  float BLKpg; // blocks per game
+  int TOV; // turnovers
+  float TOVpg; // turnovers per game
+  int PTS; // points
+  float Ppg; // points per game
+  
+    
+    struct player* next; // pointer to the next player
 };
 
 class HashTable
@@ -27,14 +50,15 @@ class HashTable
     int tableSize;
 
     // Pointer to an array containing buckets (the Hash Table)
-    node **hashTable;
+    player **hashTable;
 
   	/*
   	Method Name: createNode
   	Purpose: Create a node with data as 'key'
     return: pointer to the new node
   	*/
-    node* createNode(int key);
+    
+    player* createPlayer(int ); // unfinished... do we really need to have 32 inputs to create a player???? might create players from driver file
 
     public:
 
@@ -44,6 +68,7 @@ class HashTable
     Param: Size of the Hash Table
     return: none
     */
+    
     HashTable(int bsize);
 
     /*
@@ -51,6 +76,7 @@ class HashTable
     Purpose: perform all operations necessary to destroy class object
     return: none
     */
+    
     ~HashTable();
 
   	/*
@@ -58,13 +84,15 @@ class HashTable
   	Purpose: inserts a node with data as 'key' into the Hash Table
     return: false if 'key' already exists in the table, otherwise true
   	*/
-    bool insertItem(int key);
+    
+    bool insertPlayer(int key);
 
     /*
   	Method Name: hashFunction
   	Purpose: function to hash "key" into an index
     return: index in the Hash Table
   	*/
+    
     unsigned int hashFunction(int key);
 
     /*
@@ -72,14 +100,16 @@ class HashTable
   	Purpose: function to display the Hash Table
     return: none
   	*/
-    void printTable();
+    
+    void printPlayer();
 
     /*
   	Method Name: searchItem
   	Purpose: function to search for "key" in the Hash Table
     return: node with "key" as it's data if found, otherwise NULL
   	*/
-    node* searchItem(int key);
-};
 
+    player* searchPlayer (string key);
+
+};
 #endif
